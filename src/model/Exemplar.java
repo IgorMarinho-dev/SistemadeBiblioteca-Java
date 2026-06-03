8e57c0ff512025c7417f2134853f0db3fcfd3a2f
tallisonferreira18-sketch <tallisonferreira18@gmail.com>
2026-06-03 15:28:11 -0300

    Add files via upload

---
+++ src/model/Exemplar.java
@@ -0,0 +1,39 @@
+package model;
+
+import model.enums.StatusExemplar;
+
+public class Exemplar {
+    private int id;
+    private String codigo;
+    private StatusExemplar status;
+    private Reserva reserva;
+
+    public Exemplar(int id, String codigo, Reserva reserva){
+        this.id = id;
+        this.codigo = codigo;
+        this.status = StatusExemplar.DISPONIVEL;
+        this.reserva = reserva;
+    }
+
+    public int getId() {return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getCodigo() { return codigo; }
+    public void setCodigo(String codigo) { this.codigo = codigo; }
+
+    public StatusExemplar getStatus() { return status; }
+    public void setStatus(StatusExemplar status) { this.status = status; }
+
+    public Reserva getReserva() { return reserva; }
+    public void setReserva(Reserva reserva) { this.reserva = reserva; }
+
+    public void marcarcomoemprestado() {
+        this.status = StatusExemplar.EMPRESTADO;
+        System.out.println("Exemplar Emprestado.");
+    }
+
+    public void marcarcomodisponivel() {
+        this.status = StatusExemplar.DISPONIVEL;
+        System.out.println("Exemplar Disponível.");
+    }
+}
